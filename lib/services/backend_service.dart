88c0317d5b1a40460b7cc4006dd1b3e5cb0a4866
fidoaf <fidoaf@gmail.com>
2022-10-04 14:58:39 +0200

    Update Backend service
    Updated webservice URL.

--- lib/services/backend_service.dart
+++ lib/services/backend_service.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 class BackendService {
   static const String HOST_DOMAIN = 'script.google.com';
   static const String MAIN_ENDPOINT =
-      '/macros/s/AKfycbzb6Q-w4nct_7BrV9_yFzyI0TmevjB4SXB_AluECphXTNGLksDeCSiZZmxAGG1Yx9E-Gw/exec';
+      '/macros/s/AKfycbyldRgcCadEdDPgECDHslWR3y-Y9_xeCVL4iwCTMboYyl0O7cnZjeL5C70ACszPLZP8qw/exec';
   static const String DATA_FORMAT = 'json';
   //
   // Singleton pattern
@@ -20,30 +20,41 @@ class BackendService {
   BackendService._internal();
   //
 
+  // Temporary data
+  String? token;
+
   Future<Map<String, dynamic>> login(String? user, String? password) async {
     String? error;
     dynamic data;
 
-    var url = Uri.https(HOST_DOMAIN, MAIN_ENDPOINT, {
-      'format': DATA_FORMAT,
-      'action': 'login',
-      'user': user,
-      'password': password,
-    });
+    if (token == null) {
+      Uri url = Uri.https(HOST_DOMAIN, MAIN_ENDPOINT, {
+        'format': DATA_FORMAT,
+        'action': 'login',
+        'user': user,
+        'password': password,
+      });
 
-    // Await the http get response, then decode the json-formatted response.
-    http.Response response = await http.get(url);
-    if (response.statusCode == 200) {
-      dynamic rspData = jsonDecode(response.body);
-      if (rspData['error'] == null) {
-        data = rspData['data'];
+      // Await the http get response, then decode the json-formatted response.
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        dynamic rspData = jsonDecode(response.body);
+        if (rspData['error'] == null) {
+          data = rspData['data'];
+          token = data['token'];
+        } else {
+          error = rspData['error']['message'];
+        }
       } else {
-        error = rspData['error']['message'];
+        error =
+            'Unexpected error returned by the server: ${response.statusCode}';
       }
-    } else {
-      error = 'Unexpected error returned by the server: ${response.statusCode}';
     }
 
     return {"error": error, "data": data};
+  }
+
+  logout() {
+    token = null;
   }
 }
