adee94e15a70fb7f520b394e08c0d19aa40bcc68
fidoaf <fidoaf@gmail.com>
2022-10-04 13:50:59 +0200

    Login form validation

--- lib/view/widgets/login_page.dart
+++ lib/view/widgets/login_page.dart
@@ -22,6 +22,8 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final BackendService apiService = BackendService();
 
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
@@ -49,96 +51,60 @@ class _LoginPageState extends State<LoginPage> {
       body: Center(
         child: Padding(
             padding: const EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-                Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      '<app.title>'.translate(locale),
-                      style: const TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 30),
-                    )),
-                Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(10),
-                    child: const Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: 20),
-                    )),
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  child: TextField(
-                    controller: nameController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'User Name',
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const ProofOfConceptWidget(),
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Forgot Password',
-                  ),
-                ),
-                Container(
-                    height: 50,
-                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: ElevatedButton(
-                      child: const Text('Login'),
-                      onPressed: () {
-                        apiService
-                            .login(nameController.text, passwordController.text)
-                            .then((Map<String, dynamic> result) => {
-                                  if (result['error'] == null)
-                                    {
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const ProofOfConceptWidget(),
-                                        ),
-                                      )
-                                    }
-                                  else
-                                    {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(SnackBar(
-                                        content: Text(result['error']),
-                                        backgroundColor: Colors.red,
-                                      ))
-                                    }
-                                });
-                      },
-                    )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            child: Form(
+                key: _formKey,
+                child: ListView(
                   children: <Widget>[
-                    const Text('Does not have account?'),
-                    TextButton(
-                      child: const Text(
-                        'Sign in',
-                        style: TextStyle(fontSize: 20),
+                    Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          '<app.title>'.translate(locale),
+                          style: const TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 30),
+                        )),
+                    Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          'Sign in',
+                          style: TextStyle(fontSize: 20),
+                        )),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: TextFormField(
+                        controller: nameController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'User Name',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a valid user name';
+                          }
+                          return null;
+                        },
                       ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: TextFormField(
+                          obscureText: true,
+                          controller: passwordController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a valid password';
+                            }
+                            return null;
+                          }),
+                    ),
+                    TextButton(
                       onPressed: () {
                         Navigator.of(context).push(
                           MaterialPageRoute(
@@ -146,11 +112,72 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         );
                       },
-                    )
+                      child: const Text(
+                        'Forgot Password',
+                      ),
+                    ),
+                    Container(
+                        height: 50,
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: ElevatedButton(
+                          child: const Text('Login'),
+                          onPressed: () {
+                            // Validate form
+                            if (_formKey.currentState!.validate()) {
+                              apiService
+                                  .login(nameController.text,
+                                      passwordController.text)
+                                  .then((Map<String, dynamic> result) => {
+                                        if (result['error'] == null)
+                                          {
+                                            Navigator.of(context).push(
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const ProofOfConceptWidget(),
+                                              ),
+                                            )
+                                          }
+                                        else
+                                          {
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(SnackBar(
+                                              content: Text(result['error']),
+                                              backgroundColor: Colors.red,
+                                            ))
+                                          }
+                                      });
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Please fill all the fields'),
+                                  backgroundColor: Colors.red,
+                                ),
+                              );
+                            }
+                          },
+                        )),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text('Does not have account?'),
+                        TextButton(
+                          child: const Text(
+                            'Sign in',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    const ProofOfConceptWidget(),
+                              ),
+                            );
+                          },
+                        )
+                      ],
+                    ),
                   ],
-                ),
-              ],
-            )),
+                ))),
       ),
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _changelocale,
